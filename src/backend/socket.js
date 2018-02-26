257258cbfd8ba0dd1f0562e109764e187665a3ec
SchaellerThibault <schaeller.thibault@gmail.com>
2018-02-26 23:16:07 +0100

    see correction from poll

--- src/backend/socket.js
+++ src/backend/socket.js
@@ -102,7 +102,7 @@ const bindAdmin = (socket, user, poll, provider) => {
     });
 
     socket.on('client:admin:question:results', () => {
-        provider.emit('server:question:result', poll.questions[poll.index]);
+        provider.emit('server:question:results', poll.questions[poll.index]);
     });
 };
 
